9fd6295137b761d601ca38a18e12713527280e7e
cloeson0910-ui <cloeson0910@gmail.com>
2026-06-29 16:39:06 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,8 @@
 const SUPABASE_URL = "https://bpzzmitmbavcmcasswbu.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_YySRrYs6VqiGLmP9958PjA_dUM6MDU8";
+
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
